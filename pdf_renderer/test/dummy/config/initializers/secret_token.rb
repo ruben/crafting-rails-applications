7c83c3e76f30952f02c22514ca547de39d3d7eaa
Rubén Gil <ruben@nuatt.es>
2013-08-08 12:21:10 +0200

    Chapter 1: Initial version of PDF Renderer

---
+++ pdf_renderer/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '28c41f77e837ba42f27efddb0e3c4803d6d19622783ace3a1e230364ded4ca3c4505aa944b061b4e440fab9f9f025e018668126bc1fb84b1a395cc398acafe01'
