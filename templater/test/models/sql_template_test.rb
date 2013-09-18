ad4fa0c581cb060ebc7fc15c8103ac6ecb02ea43
Rubén Gil <ruben@nuatt.es>
2013-09-18 16:20:23 +0200

    Implemented Resolver cache

--- templater/test/models/sql_template_test.rb
+++ templater/test/models/sql_template_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class SqlTemplateTest < ActiveSupport::TestCase
 
   test "resolver returns template with the saved body" do
-    resolver = SqlTemplate::Resolver.new
-    details = {formats: [:html], locale: [:en], handlers: [:erb]}
+    resolver = SqlTemplate::Resolver.instance
+    details = { formats: [:html], locale: [:en], handlers: [:erb] }
 
     #1) Assert the resolver cannot find the template as the database is empty
-    # find_all(name, prefix, partial, details). Where are cache key and locals?
+    # find_all(name, prefix, partial, details)
     assert resolver.find_all('index', 'posts', false, details).empty?
 
     #2) Create the template in the database
@@ -30,5 +30,20 @@ class SqlTemplateTest < ActiveSupport::TestCase
     assert_equal "posts/index", template.virtual_path
     assert_match %r[SqlTemplate - \d+ - "posts/index"], template.identifier
 
+  end
+
+  test "sql template expires the cache on update" do
+    cache_key = Object.new
+    resolver = SqlTemplate::Resolver.instance
+    details = { formats: [:html], locale: [:en], handlers: [:erb] }
+
+    template =  resolver.find_all("create", "posts", false, details, cache_key ).first
+    assert_equal "body",  template.source
+
+    sql_template = sql_templates(:one)
+    sql_template.update_attributes(body: "changed body")
+
+    template =  resolver.find_all("create", "posts", false, details, cache_key ).first
+    assert_equal "changed body", template.source
   end
 end
