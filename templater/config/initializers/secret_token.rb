ed20e858bd972f65a12deebf8b4970119ad93241
Rubén Gil <ruben@nuatt.es>
2013-09-18 12:17:20 +0200

    Created templater project and SQLResolver

---
+++ templater/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Templater::Application.config.secret_key_base = '714bd88b27ab96d7d5396ee32d01b53f33b1ca8a97f50af579be0c90e412e62622150779939c66f0e9b6f4b3a6d50c5f939a15f7f4a2b849c1f3ab033f55acb0'
