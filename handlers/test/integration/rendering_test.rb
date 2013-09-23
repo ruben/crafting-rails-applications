69c9065153311bf153a3ba46f2d8d9bc13b76aed
Rubén Gil <rubengil22@gmail.com>
2013-09-23 12:22:20 +0200

    Chapter 4 : Interpolated string handler

--- handlers/test/integration/rendering_test.rb
+++ handlers/test/integration/rendering_test.rb
@@ -3,4 +3,9 @@ class RenderingTest < ActionDispatch::IntegrationTest
     get "/handlers/rb_handler"
     assert_match "This is my first <b>template handler</b>!", response.body
   end
+
+  test ".string template handler" do
+    get "/handlers/string_handler"
+    assert_match "Congratulations! You just created another template handler!", response.body
+  end
 end
